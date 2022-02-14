c7a0aa8e65e06c72b3ffd05de342779ac6e0478d
Jedi <Jedi@DESKTOP-H86PBB6>
2022-02-15 00:16:02 +0800

    Add project files.

---
+++ ClientTest/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace ClientTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtVis = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblVisPrice = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(199, 23);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(164, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert/Query";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtVis
+            // 
+            this.txtVis.Location = new System.Drawing.Point(42, 23);
+            this.txtVis.Name = "txtVis";
+            this.txtVis.Size = new System.Drawing.Size(151, 20);
+            this.txtVis.TabIndex = 2;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(42, 49);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(151, 20);
+            this.txtPeso.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "VIS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PHP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "VIS Price Now:";
+            // 
+            // lblVisPrice
+            // 
+            this.lblVisPrice.AutoSize = true;
+            this.lblVisPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVisPrice.Location = new System.Drawing.Point(322, 49);
+            this.lblVisPrice.Name = "lblVisPrice";
+            this.lblVisPrice.Size = new System.Drawing.Size(19, 20);
+            this.lblVisPrice.TabIndex = 7;
+            this.lblVisPrice.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(284, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Php";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 85);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblVisPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtVis);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "VIS Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtVis;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblVisPrice;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ClientTest/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp; // Adding websocket
+using Newtonsoft.Json.Linq; //Adding jsonj parser
+using Newtonsoft.Json;
+using System.Windows.Threading;
+
+namespace ClientTest
+{
+    public partial class Form1 : Form
+    {
+        public class Vigorus
+        {
+            [JsonProperty("php")]
+            public string peso { get; set; }
+        }
+        public class Data
+        {
+            [JsonProperty("vigorus")]
+            public Vigorus vigorus { get; set; }
+        }
+
+        public class Price
+        {
+            [JsonProperty("code")]
+            public string code { get; set; }
+
+            [JsonProperty("data")]
+            public Data data { get; set; }
+
+            [JsonProperty("message")]
+            public string message { get; set; }
+
+            [JsonProperty("success")]
+            public bool success{ get; set; }
+        }
+
+     
+
+        WebSocket ws = new WebSocket(url: "ws://localhost:3300", onMessage: onMessage);
+        string username = "guest";
+        static dynamic json;
+        static dynamic prevJson;
+        public Form1()
+        {
+            InitializeComponent();
+            ws.Connect();
+            updateMessageBox();
+        }
+
+        private async void updateMessageBox()
+        {
+            while(true)
+            {
+                if (prevJson != json)
+                {
+                    try
+                    {
+                        Price castedJson = (Price)json;
+                        addMessage((string)castedJson.data.vigorus.peso);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+
+                prevJson = json;
+                await Task.Delay(100);
+            }
+        }
+       
+        private static Task onMessage(MessageEventArgs e)
+        {
+            json = JsonConvert.DeserializeObject<Price>(e.Text.ReadToEnd());
+            return Task.FromResult(0);
+        }
+
+        private void addMessage(string peso)
+        {
+            
+            Dispatcher.CurrentDispatcher.Invoke(() =>
+            {
+                txtPeso.Text = (float.Parse(txtVis.Text) * float.Parse(peso)).ToString();
+                lblVisPrice.Text = peso;
+            });
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ws.Send("Hello");
+           //ws.Send("{	\"MessageInfo\": {		\"Username\": \"" + username + "\",		\"Message\": \" " + txtVis.Text + "\"	}}");
+            //ws.Send("{ \"MessageInfo\": { \"Username\": \"" + username + "\", \"Message\": \"" + textBox2.Text + "\" }}");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
